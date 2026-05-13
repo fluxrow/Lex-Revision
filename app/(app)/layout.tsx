2b066eeb88767c67b7c4ef88d7351b0bb1458548
fluxrow <qdfy2p4gbr@privaterelay.appleid.com>
2026-05-12 21:10:52 -0300

    Force dynamic auth layout for protected routes

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -3,6 +3,8 @@ import SignOutButton from "@/components/auth/SignOutButton";
 import { getCurrentAccount } from "@/lib/auth/account";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function AppLayout({
   children,
 }: Readonly<{
